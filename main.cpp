2f8c1bfbde43774888e75ea06336e09e27a82e9b
dimorl411341 <65544358+dimorl411341@users.noreply.github.com>
2020-05-18 17:39:46 +0400

    ADD main.cpp 2

--- main.cpp
+++ main.cpp
@@ -3,4 +3,15 @@
 using namespace std;
 
 int main()
-
+{
+	int a, b, c;
+	cout << "¬ведите a=";
+	cin >> a;
+	cout << endl;
+	cout << "¬ведите b=";
+	cin >> b;
+	cout << endl;
+	c = a + b;
+	cout << "—умма равна =" << c;
+	return 0;
+}
