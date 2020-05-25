b09bdbcddb3bacb9bbf9ed98e47998e7bdc47cec
dimorl411341 <65544358+dimorl411341@users.noreply.github.com>
2020-05-25 11:45:25 +0400

    ADD NEWWW

---
+++ main — копия — копия — копия.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Ââåäèòå a=";
+	cin >> a;
+	cout << endl;
+	cout << "Ââåäèòå b=";
+	cin >> b;
+	cout << endl;
+	c = a + b;
+	cout << "Ñóììà ðàâíà =" << c;
+	return 0;
+	//HELP ME
+	//ÏÎÌÎÃÈÒÅ
+}
