8ea44c3cefc21d8375c91393c1fe1eac099b67fa
dimorl411341 <65544358+dimorl411341@users.noreply.github.com>
2020-05-25 11:43:36 +0400

    ADD NEW

---
+++ main — копия — копия.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "¬ведите a=";
+	cin >> a;
+	cout << endl;
+	cout << "¬ведите b=";
+	cin >> b;
+	cout << endl;
+	c = a + b;
+	cout << "—умма равна =" << c;
+	return 0;
+	//HELP ME
+}
